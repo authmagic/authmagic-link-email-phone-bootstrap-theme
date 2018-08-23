9d22a57ff70b789999e8c0079cb58c54b5cf8b79
oleks <oleksandrknyga@gmail.com>
2018-08-23 20:20:37 +0300

    middleware fixes

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -2,6 +2,16 @@
 
 document.getElementById('redirect-url').value = getParameterByName('redirect') ? getParameterByName('redirect') : location.protocol + '//' + location.host + '/check.html';
 
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 function getToken() {
   var emailElement = document.getElementById('email');
   var redirectUrl = document.getElementById('redirect-url').value;
